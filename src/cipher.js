ad01d9d8af1271dbec9fda7134eafc4cd8e4bf9c
verobega <pooh_tres@hotmail.com>
2020-02-17 11:28:14 -0600

    segundoavance

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,21 @@
 const cipher = {
-  // ...
-};
+  cifrar
+}
+    function cifrar(claveTemporal, moves){
+        let resultEncode="";
+      
+      for (var i = 0; i < claveTemporal.length ;  i++){
+        let wordCipher = claveTemporal.charCodeAt(i); // convierte la letra en numero ascii
+       if (wordCipher >= 65 && wordCipher <= 90){ // condicion que debe cumplir el texto una vez convertido en ASCII
+         let formula = (wordCipher - 65 + moves) % 26 + 65; // aplica la formula a las letras
+         let newLetter = String.fromCharCode(formula); //palabra cifrada
+         
+         resultEncode += newLetter;
+       }
+      }
+      return resultEncode; //
+      
+      }
+export default cipher
 
-export default cipher;
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 import cipher from './cipher.js';
 
+let claveTemporal = "";
+let movimientos = 0;
+let cifrar = document.getElementById("clavesita");
+
+function obtenerClave() {
+    claveTemporal= document.getElementById("clavealfabetica").value;
+    movimientos = document.getElementById("moves").value;
+}
+
+cifrar.onclick= function () {
+    claveTemporal = document.getElementById('clavealfabetica').value
+    movimientos = document.getElementById('moves').value
+    let res = cipher.cifrar(claveTemporal);
+    document.getElementById('claveCifrada').value = res
+}
+
 console.log(cipher);
